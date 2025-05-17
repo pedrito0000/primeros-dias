3bdd8f1984f5238ef80299f84e541b1387ad54ad
pedrito0000 <pedrito333ggg@gmail.com>
2025-05-17 18:16:45 +0200

    ASD

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/primeros-dias/',
+  build: {
+    outDir: 'docs',
+    // si ya tienes rollupOptions, solo añade la propiedad outDir
+    rollupOptions: {
+        // si no necesitas entradas extra, esto puede omitirse,
+        // pero conviene que al menos se asegure apuntar a tu index.html:
+        input: 'index.html'
+    }
+  },
   plugins: [react()],
 })
 // https://vitejs.dev/guide/features.html#css-pre-processors
